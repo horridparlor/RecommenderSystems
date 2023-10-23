a785a651f6a6f3d28adf0d0e932f4cb8887c80fb
Eero Laine <horridparlormaster@gmail.com>
2023-10-23 20:41:40 +0300

    Dataset downloaded

--- recommender-systems/src/main/java/fi/tuni/Main.java
+++ recommender-systems/src/main/java/fi/tuni/Main.java
@@ -1,7 +1,29 @@
 package fi.tuni;
 
+import org.apache.commons.csv.CSVParser;
+
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        InputStream ratings = Main.class.getResourceAsStream("/ml-latest-small/ratings.csv");
+        BufferedReader reader = null;
+        String line = "";
+        int rowCount = 0;
+        try {
+            reader = new BufferedReader(new InputStreamReader(ratings));
+            while ((line = reader.readLine()) != null) {
+                rowCount++;
+            }
+            System.out.println(rowCount);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
