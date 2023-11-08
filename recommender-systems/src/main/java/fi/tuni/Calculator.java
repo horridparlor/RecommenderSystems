51f1ef40f243f4ba1160f4c56bfc446f40bf3719
Yilikaer Yihamujiang <yilikaer.yihamujiang@tuni.fi>
2023-11-08 14:21:26 +0200

    added aggregateAverage

--- recommender-systems/src/main/java/fi/tuni/Calculator.java
+++ recommender-systems/src/main/java/fi/tuni/Calculator.java
@@ -2,10 +2,17 @@ package fi.tuni;
 
 import org.apache.commons.math3.stat.correlation.PearsonsCorrelation;
 
+import java.util.*;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
+
+
 
 public class Calculator {
     public static double getPearsonCorrelation(User user1, User user2) {
@@ -61,6 +68,51 @@ public class Calculator {
 
         return numerator / (Math.sqrt(denominatorUser1) * Math.sqrt(denominatorUser2));
     }
+
+    public static List<Integer> aggregateAverage(List<Integer> groupUsers, int numRecommendations) {
+        // Create a map to store the sum of ratings for each movie
+        Map<Integer, Double> movieScores = new HashMap<>();
+    
+        // Count the number of users who have rated each movie
+        Map<Integer, Integer> movieCounts = new HashMap<>();
+    
+        for (int userId : groupUsers) {
+            // Get individual recommendations for the user
+            ArrayList<Similarity> similarUsers = Algorithm.getSimilarUsers(userId);
+            ArrayList<MovieRecommendation> recommendations = Algorithm.getRelevantMovies(userId, similarUsers);
+    
+            for (MovieRecommendation recommendation : recommendations) {
+                int movieId = recommendation.getId(); // Extract the movie ID from MovieRecommendation
+                double rating = recommendation.getRelevancy(); // Get the predicted rating from MovieRecommendation
+    
+                // Update the sum of ratings for the movie
+                movieScores.put(movieId, movieScores.getOrDefault(movieId, 0.0) + rating);
+                // Increment the count for the movie
+                movieCounts.put(movieId, movieCounts.getOrDefault(movieId, 0) + 1);
+            }
+        }
+    
+        // Calculate the average rating for each movie
+        Map<Integer, Double> movieAverages = new HashMap<>();
+        for (int movieId : movieScores.keySet()) {
+            double sum = movieScores.get(movieId);
+            int count = movieCounts.get(movieId);
+            double average = sum / count;
+            movieAverages.put(movieId, average);
+        }
+    
+        // Sort the aggregated recommendations by average rating in descending order
+        List<Integer> sortedRecommendations = movieAverages.entrySet().stream()
+                .sorted((entry1, entry2) -> Double.compare(entry2.getValue(), entry1.getValue()))
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+    
+        return sortedRecommendations.subList(0, Math.min(numRecommendations, sortedRecommendations.size()));
+    }
+    
+    
+
+
 
     private static double calculateMeanRating(Map<Integer, Rating> userRatings, Set<Integer> commonMovies) {
         double sum = 0;
